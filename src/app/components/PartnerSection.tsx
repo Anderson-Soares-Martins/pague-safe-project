ec3a635eb0bd2b2c2dc9af3b439cda9cc8f8ab51
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2024-09-02 16:20:48 -0300

    refact atualizado tamanho das imagens que tem no scroll

--- src/app/components/PartnerSection.tsx
+++ src/app/components/PartnerSection.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
-import bancoDoBrasil from './../assets/partners/banco-do-brasil.svg'
-import Inter from './../assets/partners/Inter.svg'
-import bradesco from './../assets/partners/bradesco.svg'
-import cartaoElo from './../assets/partners/cartao-elo.svg'
-import mastercard from './../assets/partners/mastercard.svg'
-import pixBancoCentral from './../assets/partners/pix-banco-central.svg'
-import visa from './../assets/partners/visa.svg'
-import InfiniteScroll from './infitiniScroller'
+import React from "react";
+import bancoDoBrasil from "./../assets/partners/banco-do-brasil.svg";
+import Inter from "./../assets/partners/Inter.svg";
+import bradesco from "./../assets/partners/bradesco.svg";
+import cartaoElo from "./../assets/partners/cartao-elo.svg";
+import mastercard from "./../assets/partners/mastercard.svg";
+import pixBancoCentral from "./../assets/partners/pix-banco-central.svg";
+import visa from "./../assets/partners/visa.svg";
+import InfiniteScroll from "./infitiniScroller";
 
 const partners = [
   {
     src: Inter,
-    alt: 'Partner logo 1'
+    alt: "Partner logo 1"
   },
   {
     src: bradesco,
-    alt: 'Partner logo 2'
+    alt: "Partner logo 2"
   },
   {
     src: mastercard,
-    alt: 'Partner logo 3'
+    alt: "Partner logo 3"
   },
   {
     src: bancoDoBrasil,
-    alt: 'Partner logo 4'
+    alt: "Partner logo 4"
   },
   {
     src: pixBancoCentral,
-    alt: 'Partner logo 5'
+    alt: "Partner logo 5"
   },
   {
     src: visa,
-    alt: 'Partner logo 6'
+    alt: "Partner logo 6"
   },
   {
     src: cartaoElo,
-    alt: 'Partner logo 7'
+    alt: "Partner logo 7"
   }
-]
+];
 
 const PartnerSection: React.FC = () => {
   return (
@@ -48,13 +48,13 @@ const PartnerSection: React.FC = () => {
       </h2>
 
       <InfiniteScroll speed="fast">
-        <ul className="flex gap-8 items-center min-w-max">
+        <ul className="flex gap-4 sm:gap-8 items-center min-w-max">
           {[...partners, ...partners].map((partner, index) => (
             <li
               key={index}
-              className="flex flex-col self-stretch my-auto rounded-none w-[5%]"
+              className="flex flex-col self-stretch my-auto rounded-none w-[4%]"
             >
-              <div className="flex flex-col justify-center px-10 py-6 bg-teal-50 rounded-xl h-28">
+              <div className="flex flex-col justify-center px-4 py-3 sm:px-10 sm:py-6 bg-teal-50 rounded-xl h-20 sm:h-24 md:h-28">
                 <img
                   loading="lazy"
                   src={partner.src.src}
@@ -67,7 +67,7 @@ const PartnerSection: React.FC = () => {
         </ul>
       </InfiniteScroll>
     </section>
-  )
-}
+  );
+};
 
-export default PartnerSection
+export default PartnerSection;
