c4467b646a8dd9618d9e656879b4e027a12fd4f1
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2024-10-09 16:32:48 -0300

    feat: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -52,7 +53,10 @@ export default function RootLayout({
           href="/icon-512x512.png"
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
